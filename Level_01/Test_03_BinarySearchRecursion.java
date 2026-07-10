2ad7d021580069f4699fb62520d9e96c4fc5969a
parmendra05 <parmendrakumar718@gmail.com>
2026-07-10 08:03:25 +0530

    Avoid integer overflow, Binary Search by Recursion

--- Level_01/Test_03_BinarySearchRecursion.java
+++ Level_01/Test_03_BinarySearchRecursion.java
@@ -1,11 +1,22 @@
 package Level_01;
+/*
+Common interview question is:
+Why do we calculate the middle as  low + (high - low) / 2  instead of (low + high) / 2  .
 
+✅ Both formulas calculate the same middle index.
+✅ (low + high) / 2 can cause integer overflow. (int limit Maximum value: 2,147,483,647)
+✅ low + (high - low) / 2 is overflow-safe.
+✅ It is the recommended approach in Java and most programming languages.
+
+Integer overflow occurs when a calculation exceeds the limit of the int data type, producing an incorrect result.
+*/
 public class Test_03_BinarySearchRecursion {
     static int binarySearch(int[] arr, int low, int high, int target){
 
         if(low > high) return -1;
 
-        int mid = (low + high)/2;
+        //int mid = (low + high)/2;
+        int mid = low + ( high - low)/2;
 
         if(arr[mid] ==target) return mid;
 
